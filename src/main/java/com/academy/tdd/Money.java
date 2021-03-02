ab92ed4a56c48214f03fa9dffeb7180f4db50f1d
Nika Avramchuk <nika.avramchuk@gmail.com>
2021-03-02 16:07:52 +0100

    create currency method and constructor in the super class

--- src/main/java/com/academy/tdd/Money.java
+++ src/main/java/com/academy/tdd/Money.java
@@ -2,13 +2,19 @@ package com.academy.tdd;
 
 abstract class Money {
     protected int amount;
+    protected String currency;
+
+    public Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
 
     public static Money dollar(int amount) {
-        return new Dollar(amount);
+        return new Dollar(amount, "USD");
     }
 
     public static Money franc(int amount) {
-        return new Franc(amount);
+        return new Franc(amount, "CHF");
     }
 
     public boolean equals(Object object) {
@@ -17,4 +23,8 @@ abstract class Money {
     }
 
     public abstract Money times(int multiplier);
+
+    public String currency() {
+        return currency;
+    };
 }
