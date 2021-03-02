843755b1d1804269933af8d2f9ec62eb4a41e901
Nika Avramchuk <nika.avramchuk@gmail.com>
2021-03-02 15:14:26 +0100

    create Money class to reduce code duplication

---
+++ src/main/java/com/academy/tdd/Money.java
@@ -0,0 +1,10 @@
+package com.academy.tdd;
+
+public class Money {
+    protected int amount;
+
+    public boolean equals(Object object) {
+        Money money = (Money) object;
+        return amount == money.amount && this.getClass().equals(money.getClass());
+    }
+}
