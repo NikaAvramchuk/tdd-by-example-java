e8786e707b2f5d602a4908e27198891b38175900
Nika Avramchuk <nika.avramchuk@gmail.com>
2021-03-04 14:49:39 +0100

    delete unnecessary import

--- src/main/java/com/academy/tdd/Bank.java
+++ src/main/java/com/academy/tdd/Bank.java
@@ -4,6 +4,7 @@ import java.util.Hashtable;
 
 public class Bank {
     private Hashtable<Pair, Integer> hashtable = new Hashtable<>();
+
     public Money reduce(Expression source, String currency) {
        return source.reduce(this, currency);
     }
@@ -14,7 +15,7 @@ public class Bank {
         return hashtable.get(new Pair(from,to));
     }
 
-    public void addRate(String from, String to, int rate) {
+    void addRate(String from, String to, int rate) {
         hashtable.put(new Pair(from,to), rate);
     }
 }
