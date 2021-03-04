56f36ef755921a17b4c2c3b9e63fc341191ff0b5
Nika Avramchuk <nika.avramchuk@gmail.com>
2021-03-04 11:17:09 +0100

    create class Sum that implements Expression

---
+++ src/main/java/com/academy/tdd/Sum.java
@@ -0,0 +1,16 @@
+package com.academy.tdd;
+
+public class Sum implements Expression {
+    Money augend;
+    Money addend;
+
+    public Sum(Money augend, Money addend) {
+        this.augend = augend;
+        this.addend = addend;
+    }
+
+    public Money reduce(String currency) {
+        int amount = augend.amount + addend.amount;
+        return new Money(amount, currency);
+    }
+}
