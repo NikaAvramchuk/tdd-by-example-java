bef599b2a3b6f56d86db25ab1a4fe10260d503ab
Nika Avramchuk <nika.avramchuk@gmail.com>
2021-03-04 11:53:44 +0100

    add hashtable with currency pairs as a key and rate as value to Bank and method for adding rate

--- src/main/java/com/academy/tdd/Bank.java
+++ src/main/java/com/academy/tdd/Bank.java
@@ -1,11 +1,21 @@
 package com.academy.tdd;
 
+import java.util.HashMap;
+import java.util.Hashtable;
+
 public class Bank {
+    private Hashtable<Pair, Integer> hashtable = new Hashtable<>();
     public Money reduce(Expression source, String currency) {
        return source.reduce(this, currency);
     }
 
     int rate(String from, String to) {
-        return (from.equals("CHF") && to.equals("USD")) ? 2:1;
+        if(from.equals(to))
+            return 1;
+        return hashtable.get(new Pair(from,to));
+    }
+
+    public void addRate(String from, String to, int rate) {
+        hashtable.put(new Pair(from,to), rate);
     }
 }
