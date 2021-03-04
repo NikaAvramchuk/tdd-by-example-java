217f5fa86bf993504304509a4ee2b59549c500b0
Nika Avramchuk <nika.avramchuk@gmail.com>
2021-03-04 11:52:42 +0100

    create test fro checking reducing money with different and same currencies

--- src/test/java/com/academy/tdd/MoneyTest.java
+++ src/test/java/com/academy/tdd/MoneyTest.java
@@ -65,4 +65,24 @@ public class MoneyTest {
         Money result = bank.reduce(Money.dollar(1),"USD");
         assertEquals(result,Money.dollar(1));
     }
+
+    @Test
+    public void testReduceMoneyDifferentCurrencies() {
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+        Money result = bank.reduce(Money.franc(2), "USD");
+        assertEquals(Money.dollar(1),result);
+    }
+
+    @Test
+    public void testReduceMoneySameCurrencies() {
+        Bank bank = new Bank();
+        Money result = bank.reduce(Money.dollar(1), "USD");
+        assertEquals(Money.dollar(1),result);
+    }
+
+    @Test
+    public void testIdentityRate() {
+        assertEquals(1, new Bank().rate("USD", "USD"));
+    }
 }
