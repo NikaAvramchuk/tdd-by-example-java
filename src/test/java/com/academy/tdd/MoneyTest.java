466fa96b5d248de1f1f87744c43036139e88b2ea
Nika Avramchuk <nika.avramchuk@gmail.com>
2021-03-04 11:16:30 +0100

    create test for reducing Expression (sum)

--- src/test/java/com/academy/tdd/MoneyTest.java
+++ src/test/java/com/academy/tdd/MoneyTest.java
@@ -2,8 +2,6 @@ package com.academy.tdd;
 
 import org.testng.annotations.Test;
 
-import java.beans.Expression;
-
 import static org.testng.Assert.*;
 
 public class MoneyTest {
@@ -35,11 +33,36 @@ public class MoneyTest {
         assertEquals("CHF", Money.franc(1).currency());
     }
 
+    @Test
     public void testSimpleAddition() {
         Money five = Money.dollar(5);
         Expression sum = five.plus(five);
         Bank bank = new Bank();
         Money reduced = bank.reduce(sum, "USD");
         assertEquals(Money.dollar(10), reduced);
+    }
+
+    @Test
+    public void testPlusReturnsSum() {
+        Money five = Money.dollar(5);
+        Expression result = five.plus(five);
+        Sum sum = (Sum) result;
+        assertEquals(five, sum.augend);
+        assertEquals(five, sum.addend);
+    }
+
+    @Test
+    public void testReduceSum() {
+        Expression sum = new Sum(Money.dollar(3), Money.dollar(4));
+        Bank bank = new Bank();
+        Money reduce = bank.reduce(sum, "USD");
+        assertEquals(reduce,Money.dollar(7));
+    }
+
+    @Test
+    public void testReduceMoney() {
+        Bank bank = new Bank();
+        Money result = bank.reduce(Money.dollar(1),"USD");
+        assertEquals(result,Money.dollar(1));
     }
 }
